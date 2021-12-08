dd48b4b37e4875bd8684b87720d6417335f12c41
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2021-12-07 21:48:46 -0300

    style: criado o input search service

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,13 +5,36 @@ import styles from './styles.module.css'
 import { Header } from '../../components/Header'
 import { CardService } from '../../components/CardService'
 
+import searchIcon from '../../assets/search.svg'
+
 export function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
         <section>
-          <h2 className='gone'>Lista de serviços</h2>
+          <form>
+            <div className={styles.inputGroup}>
+              <img
+                src={searchIcon}
+                alt="Lupa de pesquisa"
+                className={styles.searchIcon}
+              />
+              <label htmlFor="search-service" className="gone">
+                Pesquise por algum serviço
+              </label>
+              <input
+                type="text"
+                className={styles.inputSearch}
+                id="search-service"
+                name="search-service"
+                placeholder="Pesquise por aqui por seu serviço"
+              />
+            </div>
+          </form>
+        </section>
+        <section>
+          <h2 className="gone">Lista de serviços</h2>
           <ul className={styles.listServices}>
             <li className={styles.cardItem}>
               <CardService />
